9bf7f55c0f9e70a6fdf52853d29894930e06490d
hui-12 <laiyh091204@gmail.com>
2023-12-06 12:52:49 +0800

    add payment_method, payment_successful, planning_summary, play, profile

---
+++ app/src/main/java/com/example/groupassignment/payment_successful.java
@@ -0,0 +1,14 @@
+package com.example.groupassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class payment_successful extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment_successful);
+    }
+}
