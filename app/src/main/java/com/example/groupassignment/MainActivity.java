e57a1c6a36b3ea844d015a7933c4378e05dc34d7
hui-12 <laiyh091204@gmail.com>
2023-12-24 01:14:02 +0800

    payment page add intent function to main, favourite, history, profile, and  payment successful

--- app/src/main/java/com/example/groupassignment/MainActivity.java
+++ app/src/main/java/com/example/groupassignment/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this,airline.class);
+        Intent intent = new Intent(this,info_flight.class);
         startActivity(intent);
 
     }
