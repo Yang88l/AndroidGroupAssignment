689b9ea952f447b4bd35993b57ad62e2a4f493a6
Yang88l <96825030+Yang88l@users.noreply.github.com>
2023-12-24 23:22:43 +0800

    fix intent of message

--- app/src/main/java/com/example/groupassignment/message.java
+++ app/src/main/java/com/example/groupassignment/message.java
@@ -10,61 +10,34 @@ import android.widget.ImageButton;
 import android.widget.Button;
 
 public class message extends AppCompatActivity {
-
-    private Button button = findViewById(R.id.notification);
-    private ImageButton button2;
-    private ImageButton button3;
-    private ImageButton button4;
-    private ImageButton button5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.message);
 
-        button2 = findViewById(R.id.imageButton11);
-        button3 = findViewById(R.id.imageButton8);
-        button4 = findViewById(R.id.imageButton9);
-        button5 = findViewById(R.id.imageButton10);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(message.this, notification.class);
-                startActivity(intent);
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(message.this, main.class);
-                startActivity(intent);
-            }
-        });
-
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(message.this, my_favourite.class);
-                startActivity(intent);
-            }
-        });
-
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(message.this, plan_history.class);
-                startActivity(intent);
-            }
-        });
-
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(message.this, profile.class);
-                startActivity(intent);
-            }
-        });
+    }
+    public void home(View view) {
+        Intent intent = new Intent(this,main.class);
+        startActivity(intent);
     }
 
+    public void heart(View view) {
+        Intent intent = new Intent(this,my_favourite.class);
+        startActivity(intent);
+    }
+
+    public void history(View view) {
+        Intent intent = new Intent(this,book_history.class);
+        startActivity(intent);
+    }
+
+    public void profile(View view) {
+        Intent intent = new Intent(this,profile.class);
+        startActivity(intent);
+    }
+
+    public void notification(View view) {
+        Intent intent = new Intent(this,notification.class);
+        startActivity(intent);
+    }
 }
