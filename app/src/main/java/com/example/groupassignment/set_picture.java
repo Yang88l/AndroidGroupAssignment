0c84c95ddc0b3d6a3dfb9c478131833a9e955e19
Yang88l <96825030+Yang88l@users.noreply.github.com>
2023-12-24 23:37:01 +0800

    fix intent of set_picture

--- app/src/main/java/com/example/groupassignment/set_picture.java
+++ app/src/main/java/com/example/groupassignment/set_picture.java
@@ -8,153 +8,39 @@ import android.view.View;
 import android.widget.ImageButton;
 
 public class set_picture extends AppCompatActivity {
-
-    private ImageButton button;
-    private ImageButton button2;
-    private ImageButton button3;
-    private ImageButton button4;
-    private ImageButton button5;
-    private ImageButton button6;
-    private ImageButton button7;
-    private ImageButton button8;
-    private ImageButton button9;
-    private ImageButton button10;
-    private ImageButton button11;
-    private ImageButton button12;
-    private ImageButton button13;
-    private ImageButton button14;
-    private ImageButton button15;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_picture);
 
-        button = findViewById(R.id.imageButton16);
-        button2 = findViewById(R.id.imageButton);
-        button3 = findViewById(R.id.imageButton3);
-        button4 = findViewById(R.id.imageButton4);
-        button5 = findViewById(R.id.imageButton5);
-        button6 = findViewById(R.id.imageButton6);
+    }
 
+    public void camera(View view) {
+        //open camera
+    }
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(set_picture.this, edit_profile.class);
-                startActivity(intent);
-            }
-        });
+    public void profile1(View view) {
+        Intent intent = new Intent(this,edit_profile.class);
+        startActivity(intent);
+    }
 
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(set_picture.this, edit_profile.class);
-                startActivity(intent);
-            }
-        });
+    public void profile2(View view) {
+        Intent intent = new Intent(this,edit_profile.class);
+        startActivity(intent);
+    }
 
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(set_picture.this, edit_profile.class);
-                startActivity(intent);
-            }
-        });
+    public void profile3(View view) {
+        Intent intent = new Intent(this,edit_profile.class);
+        startActivity(intent);
+    }
 
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(set_picture.this, edit_profile.class);
-                startActivity(intent);
-            }
-        });
+    public void profile4(View view) {
+        Intent intent = new Intent(this,edit_profile.class);
+        startActivity(intent);
+    }
 
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(set_picture.this, edit_profile.class);
-                startActivity(intent);
-            }
-        });
-
-        button6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(set_picture.this, edit_profile.class);
-                startActivity(intent);
-            }
-        });
-
-        button7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(set_picture.this, edit_profile.class);
-                startActivity(intent);
-            }
-        });
-
-        button8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(set_picture.this, edit_profile.class);
-                startActivity(intent);
-            }
-        });
-
-        button9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(set_picture.this, edit_profile.class);
-                startActivity(intent);
-            }
-        });
-
-        button10.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(set_picture.this, edit_profile.class);
-                startActivity(intent);
-            }
-        });
-
-        button11.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(set_picture.this, edit_profile.class);
-                startActivity(intent);
-            }
-        });
-
-        button12.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(set_picture.this, edit_profile.class);
-                startActivity(intent);
-            }
-        });
-
-        button13.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(set_picture.this, edit_profile.class);
-                startActivity(intent);
-            }
-        });
-
-        button14.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(set_picture.this, edit_profile.class);
-                startActivity(intent);
-            }
-        });
-
-        button15.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(set_picture.this, edit_profile.class);
-                startActivity(intent);
-            }
-        });
+    public void profile5(View view) {
+        Intent intent = new Intent(this,edit_profile.class);
+        startActivity(intent);
     }
 }
