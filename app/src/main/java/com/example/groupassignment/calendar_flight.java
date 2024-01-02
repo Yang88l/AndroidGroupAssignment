e7fffa7c435bcc87b3ad2e3932abb88262d9b38c
Yang88l <96825030+Yang88l@users.noreply.github.com>
2024-01-02 09:49:06 +0800

    calendar flight

--- app/src/main/java/com/example/groupassignment/calendar_flight.java
+++ app/src/main/java/com/example/groupassignment/calendar_flight.java
@@ -3,6 +3,7 @@ package com.example.groupassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,30 +16,31 @@ public class calendar_flight extends AppCompatActivity {
     private CalendarView calendarView;
     private String date;
     private int user_id;
+    private com.example.groupassignment.dbmanager_login_history dbmanager_login_history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_flight);
+    }
 
-        button = findViewById(R.id.confirm);
+    public void confirm(View view) {
+        EditText date = findViewById(R.id.date);
+        //get user id
+        dbmanager_login_history = new dbmanager_login_history(this);
+        dbmanager_login_history.open();
+        Cursor cursor_login = dbmanager_login_history.fetch();
+        cursor_login.moveToLast();
+        int user_id=Integer.parseInt(cursor_login.getString(1));
+        dbmanager_login_history.close();
 
         dbmanager_flight = new dbmanager_flight(this);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //date = calendarView.getText().toString();
-                //date="27/12/2023";
-                EditText date = findViewById(R.id.date);
-                user_id=1;
-
-                dbmanager_flight.open();
-                dbmanager_flight.update(null,null,null, String.valueOf(date),null,null);
-                dbmanager_flight.close();
-                Intent intent = new Intent(calendar_flight.this, time_flight.class);
-                startActivity(intent);
-            }
-        });
+        dbmanager_flight.open();
+        Cursor cursor = dbmanager_flight.fetch(1);
+        cursor.moveToLast();
+        dbmanager_flight.update(Integer.parseInt(cursor.getString(0)),null,null, String.valueOf(date),null,user_id);
+        dbmanager_flight.close();
+        Intent intent = new Intent(calendar_flight.this, time_flight.class);
+        startActivity(intent);
     }
 }
