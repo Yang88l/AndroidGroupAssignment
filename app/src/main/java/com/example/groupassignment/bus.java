5e50df0f5aae40a30d741c18a2900c45557142d5
hui-12 <laiyh091204@gmail.com>
2024-01-02 08:50:17 +0800

    add intent fucntion for bus company's button and navigation bar

--- app/src/main/java/com/example/groupassignment/bus.java
+++ app/src/main/java/com/example/groupassignment/bus.java
@@ -2,6 +2,7 @@ package com.example.groupassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -21,17 +22,47 @@ public class bus extends AppCompatActivity {
         dbmanager_bus.open();
         dbmanager_bus.update(null, null, "Soutern", null);
         dbmanager_bus.close();
+
+        Intent intent = new Intent(this,seat_bus.class);
+        startActivity(intent);
+
     }
 
     public void mayang_sari(View view) {
         dbmanager_bus.open();
         dbmanager_bus.update(null, null, "Mayang Sari", null);
         dbmanager_bus.close();
+
+        Intent intent = new Intent(this,seat_bus.class);
+        startActivity(intent);
     }
 
     public void city_express(View view) {
         dbmanager_bus.open();
         dbmanager_bus.update(null, null, "City Express", null);
         dbmanager_bus.close();
+
+        Intent intent = new Intent(this,seat_bus.class);
+        startActivity(intent);
+    }
+
+    public void main(View view) {
+        Intent intent = new Intent(this,main.class);
+        startActivity(intent);
+    }
+
+    public void profile(View view) {
+        Intent intent = new Intent(this,profile.class);
+        startActivity(intent);
+    }
+
+    public void history(View view) {
+        Intent intent = new Intent(this,book_history.class);
+        startActivity(intent);
+    }
+
+    public void heart(View view) {
+        Intent intent = new Intent(this, my_favourite.class);
+        startActivity(intent);
     }
 }
