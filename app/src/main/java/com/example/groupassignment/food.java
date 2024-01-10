a870136cea2b1aee6ee8759c49f39e47b08e2214
xitra <workcar123@gmail.com>
2024-01-11 00:56:25 +0800

    all favourite aded

--- app/src/main/java/com/example/groupassignment/food.java
+++ app/src/main/java/com/example/groupassignment/food.java
@@ -3,11 +3,15 @@ package com.example.groupassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class food extends AppCompatActivity {
+    private com.example.groupassignment.dbmanager_favourite dbmanager_favourite;
+    private com.example.groupassignment.dbmanager_login_history dbmanager_login_history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +45,46 @@ public class food extends AppCompatActivity {
         intent.putExtra("from", "food");
         intent.putExtra("_id", 3);
         startActivity(intent);
+    }
+    public void cielo_kl_favourite(View view) {
+        ImageView picture = findViewById(R.id.imageButton12);
+        picture.setImageResource(getResources().getIdentifier(("love_red"), "drawable", getPackageName()));
+        dbmanager_favourite = new dbmanager_favourite(this);
+        dbmanager_favourite.open();
+        dbmanager_favourite.insert(getUserID(), "hotel", 1);
+        dbmanager_favourite.close();
+        main.updateVersion();
+    }
+
+    public void horizon_grill_favourite(View view) {
+        ImageView picture = findViewById(R.id.imageButton22);
+        picture.setImageResource(getResources().getIdentifier(("love_red"), "drawable", getPackageName()));
+        dbmanager_favourite = new dbmanager_favourite(this);
+        dbmanager_favourite.open();
+        dbmanager_favourite.insert(getUserID(), "hotel", 2);
+        dbmanager_favourite.close();
+        main.updateVersion();
+    }
+
+    public void sky_bar_favourite(View view) {
+        ImageView picture = findViewById(R.id.imageButton7);
+        picture.setImageResource(getResources().getIdentifier(("love_red"), "drawable", getPackageName()));
+        dbmanager_favourite = new dbmanager_favourite(this);
+        dbmanager_favourite.open();
+        dbmanager_favourite.insert(getUserID(), "hotel", 3);
+        dbmanager_favourite.close();
+        main.updateVersion();
+    }
+
+    public int getUserID(){
+        dbmanager_login_history = new dbmanager_login_history(this);
+        dbmanager_login_history.open();
+        Cursor cursor_login = dbmanager_login_history.fetch();
+        cursor_login.moveToLast();
+        int user_id=Integer.parseInt(cursor_login.getString(1));
+        cursor_login.close();
+        dbmanager_login_history.close();
+        main.updateVersion();
+        return user_id;
     }
 }
