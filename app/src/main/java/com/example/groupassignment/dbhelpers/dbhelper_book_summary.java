33251b0f4a3eaf29b504016661b3a6a344e9a6e8
Yang88l <96825030+Yang88l@users.noreply.github.com>
2024-01-14 23:17:40 +0800

    book summary and history

--- app/src/main/java/com/example/groupassignment/dbhelpers/dbhelper_book_summary.java
+++ app/src/main/java/com/example/groupassignment/dbhelpers/dbhelper_book_summary.java
@@ -13,6 +13,7 @@ public class dbhelper_book_summary extends SQLiteOpenHelper {
     public static final String TYPE = "type";
     public static final String ACTIVITY_ID = "activity_id";
     public static final String USER_ID = "user_id";
+    public static final String LOGIN_ID = "login_id";
 
     // Database Information
     static final String DB_NAME = "JOURNALDEV_TRAVEL_BOOKING.DB";
@@ -26,7 +27,8 @@ public class dbhelper_book_summary extends SQLiteOpenHelper {
             + BOOK_SUMMARY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
             + TYPE + " TEXT NOT NULL, "
             + ACTIVITY_ID + " INTEGER NOT NULL, "
-            + USER_ID + " INTEGER NOT NULL "
+            + USER_ID + " INTEGER NOT NULL, "
+            + LOGIN_ID + " INTEGER NOT NULL "
             + ")";
 
     public dbhelper_book_summary(Context context) {
