63875e9ed13585747f59580b618310056f8ec518
xitra <workcar123@gmail.com>
2024-01-15 22:45:53 +0800

    set picture fixed

--- app/src/main/java/com/example/groupassignment/set_picture.java
+++ app/src/main/java/com/example/groupassignment/set_picture.java
@@ -14,8 +14,7 @@ import com.example.groupassignment.dbmanagers.dbmanager_user;
 
 public class set_picture extends AppCompatActivity {
     private com.example.groupassignment.dbmanagers.dbmanager_user dbmanager_user;
-      private VideoView bg;
-    private int currentPosition;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,21 +22,6 @@ public class set_picture extends AppCompatActivity {
 
         //Top Navigation
         BaseActivity.setupToolbar(this);
-
-        //Background
-  bg = findViewById(R.id.background);
-
-        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.background;
-        Uri videoUri = Uri.parse(videoPath);
-        bg.setVideoURI(videoUri);
-
-        bg.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                mp.setLooping(true);
-                bg.start();
-            }
-        });
     }
 
     public void camera(View view) {
@@ -76,37 +60,5 @@ public class set_picture extends AppCompatActivity {
         main.updateVersion();
         Intent intent = new Intent(this, profile.class);
         startActivity(intent);
-    }
-    @Override
-    protected void onResume() {
-        // Resume the video playback from the saved position
-        bg.seekTo(currentPosition);
-        bg.start();
-        super.onResume();
-    }
-    @Override
-    protected void onRestart() {
-        bg.start();
-        super.onRestart();
-    }
-    @Override
-    protected void onPause() {
-        // Save the current playback position
-        currentPosition = bg.getCurrentPosition();
-        // Pause the video playback
-        bg.pause();
-        super.onPause();
-    }
-    @Override
-    protected void onDestroy() {
-        bg.stopPlayback();
-        super.onDestroy();
-    }
-
-    @Override
-    public void onBackPressed() {
-        currentPosition = bg.getCurrentPosition();
-        bg.pause();
-        super.onBackPressed();
     }
 }
