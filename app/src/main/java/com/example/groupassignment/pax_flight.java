e007d6b19389dff51b83f1b956502b0127c57c30
hui-12 <laiyh091204@gmail.com>
2024-01-18 00:51:41 +0800

    flight pax detect function

--- app/src/main/java/com/example/groupassignment/pax_flight.java
+++ app/src/main/java/com/example/groupassignment/pax_flight.java
@@ -51,24 +51,31 @@ public class pax_flight extends AppCompatActivity {
     public void buy(View view) {
         EditText ticket_amount = findViewById(R.id.editTextNumber2);
         int amount = Integer.parseInt(ticket_amount.getText().toString());
-        /*
+
         EditText adult_amount = findViewById(R.id.adult_amount);
         int adult = Integer.parseInt(adult_amount.getText().toString());
         EditText kid_amount = findViewById(R.id.kid_amount);
         int kid = Integer.parseInt(kid_amount.getText().toString());
-*/
 
-        dbmanager_flight = new dbmanager_flight(this);
-        dbmanager_flight.open();
-        Cursor cursor = dbmanager_flight.fetch();
-        cursor.moveToLast();
-        dbmanager_flight.update(Integer.parseInt(cursor.getString(0)), null, null,null, null,-1, amount);
-        cursor.close();
-        dbmanager_flight.close();
-        main.updateVersion();
 
-        Intent intent = new Intent(pax_flight.this, info_flight.class);
-        startActivity(intent);
+        int sum = adult+kid;;
+
+        if(sum == amount) {
+            dbmanager_flight = new dbmanager_flight(this);
+            dbmanager_flight.open();
+            Cursor cursor = dbmanager_flight.fetch();
+            cursor.moveToLast();
+            dbmanager_flight.update(Integer.parseInt(cursor.getString(0)), null, null, null, null, -1, amount);
+            cursor.close();
+            dbmanager_flight.close();
+            main.updateVersion();
+
+            Intent intent = new Intent(pax_flight.this, info_flight.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(this, "The sum of adult and kid tickets (" + sum + ") doesn't match the total amount.", Toast.LENGTH_SHORT).show();
+        }
     }
     public void notification(View view) { startActivity(new Intent(this, notification.class));}
     public void home(View view) {
